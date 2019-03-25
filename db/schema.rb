e2c18fdc3061c53d7b31e3a3acf3fae56e8a0c77
taka027 <kanekotakashi2009@gmail.com>
2019-03-25 15:30:53 +0000

    blog functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_133031) do
+ActiveRecord::Schema.define(version: 2019_03_25_150950) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
