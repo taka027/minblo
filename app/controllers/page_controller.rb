258a66e34f362b948da3cdee277ed1239c0b397c
taka027 <kanekotakashi2009@gmail.com>
2019-03-29 09:02:51 +0000

    created_at

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,21 +1,21 @@
 class PageController < ApplicationController
   def index
     @categories = Category.all
-    @target_blog = Blog.where(category_id: 1).order('updated_at ASC').first
-    @blogs = Blog.where(category_id: 1).order('updated_at ASC')
+    @target_blog = Blog.where(category_id: 1).order('created_at ASC').first
+    @blogs = Blog.where(category_id: 1).order('created_at ASC')
   end
   
   def category
     @categories = Category.all
-    @target_blog = Blog.where(category_id: params[:id]).order('updated_at ASC').first
-    @blogs = Blog.where(category_id: params[:id]).order('updated_at ASC')
+    @target_blog = Blog.where(category_id: params[:id]).order('created_at ASC').first
+    @blogs = Blog.where(category_id: params[:id]).order('created_at ASC')
     render template: 'page/index'
   end
   
   def page
     @categories = Category.all
     @target_blog = Blog.where(category_id: params[:category_id]).where(id: params[:id]).first
-    @blogs = Blog.where(category_id: params[:category_id]).order('updated_at ASC')
+    @blogs = Blog.where(category_id: params[:category_id]).order('created_at ASC')
     render template: 'page/index'
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,10 @@
 class Blog < ApplicationRecord
   belongs_to :category
   def prev
-    Blog.where(category_id: self.category_id).where("updated_at < ?", self.updated_at).order("updated_at DESC").first
+    Blog.where(category_id: self.category_id).where("created_at < ?", self.created_at).order("created_at DESC").first
   end
  
   def next
-    Blog.where(category_id: self.category_id).where("updated_at > ?", self.updated_at).order("updated_at ASC").first
+    Blog.where(category_id: self.category_id).where("created_at > ?", self.created_at).order("created_at ASC").first
   end
 end
