32fd17e356b9781d5f17ecd253be800e4a7fc1a9
taka027 <kanekotakashi2009@gmail.com>
2019-04-05 07:01:53 +0000

    title

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,12 +4,14 @@ class PageController < ApplicationController
     @target_blog = Blog.order('category_id ASC').order('created_at ASC').first
     #@target_blog = Blog.where(category_id: 1).order('created_at ASC').first
     @blogs = Blog.where(category_id: @target_blog.category_id).order('created_at ASC')
+    set_title
   end
   
   def category
     @categories = Category.all
     @target_blog = Blog.where(category_id: params[:id]).order('created_at ASC').first
     @blogs = Blog.where(category_id: params[:id]).order('created_at ASC')
+    set_title
     render template: 'page/index'
   end
   
@@ -17,6 +19,15 @@ class PageController < ApplicationController
     @categories = Category.all
     @target_blog = Blog.where(category_id: params[:category_id]).where(id: params[:id]).first
     @blogs = Blog.where(category_id: params[:category_id]).order('created_at ASC')
+    set_title
     render template: 'page/index'
+  end
+
+  private
+
+  def set_title
+    if !@target_blog.nil?
+      @title = @target_blog.title
+    end
   end
 end
