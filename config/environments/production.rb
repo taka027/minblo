fb6ed181bc48a91c7a518f23a7a4d77f9f585a75
taka027 <kanekotakashi2009@gmail.com>
2019-04-05 05:04:59 +0000

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => ENV["SENDGRID_USERNAME"],
-  　　　　　　　　:password => ENV["SENDGRID_PASSWORD"],
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
       :domain => "heroku.com",
       :address => "smtp.sendgrid.net",
       :port => 587,
